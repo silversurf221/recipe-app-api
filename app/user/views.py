f89d5fa0f72a640990ff063ab3222526c7912a69
Keenan Crawford <kpcrawford12@gmail.com>
2022-10-14 19:48:09 -0500

    Added unauthorized user test and private class

--- app/user/views.py
+++ app/user/views.py
@@ -4,7 +4,6 @@ Views for the user API.
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
 from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
